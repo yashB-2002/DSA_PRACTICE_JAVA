d2555a4d5030226fa59c8916de5a6f085372febe
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2022-03-02 21:31:33 +0530

    Sort 0,1,2 in Array.

---
+++ sort012.java
@@ -0,0 +1,36 @@
+public class sort012 {
+
+    public static void sort_012(int[] arr){
+        int n=arr.length;
+        int i, cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for (i = 0; i < n; i++) {
+            switch (arr[i]) {
+            case 0:
+                cnt0++;
+                break;
+            case 1:
+                cnt1++;
+                break;
+            case 2:
+                cnt2++;
+                break;
+            }
+        }
+        i = 0;
+        while (cnt0 > 0) {
+            arr[i++] = 0;
+            cnt0--;
+        }
+        while (cnt1 > 0) {
+            arr[i++] = 1;
+            cnt1--;
+        }
+        while (cnt2 > 0) {
+            arr[i++] = 2;
+            cnt2--;
+        }
+        
+        
+    }
+
+}
