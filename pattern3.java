ed0c3145c842184849ab8fa51e85e3f1345c9316
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2022-03-05 03:28:58 +0530

    Solid Rhombus pattern.

---
+++ pattern3.java
@@ -0,0 +1,21 @@
+public class pattern3{
+    
+//       *****
+//      *****
+//     *****
+//    *****
+//   *****
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n; i++) {
+            //spaces
+            for(int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= n; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
