db5a22474e0148d678191354d2f3721c4b0c7b6c
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2022-03-10 15:38:19 +0530

    complete square root of number with precision.

--- binarySearch/sqRoot.java
+++ binarySearch/sqRoot.java
@@ -1,24 +1,38 @@
 public class sqRoot {
-    public static int binarySearch( int s, int e, int num) {
-        int ans= -1;
-        while(s <= e) {
-            int mid = s + (e-s)/2;
-            if((mid * mid )== num ) {
+    public static int binarySearch(int s, int e, int num) {
+        int ans = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if ((mid * mid) == num) {
                 return mid;
-            }
-            else if ( mid*mid < num ) {
+            } else if (mid * mid < num) {
                 ans = mid;
                 s = mid + 1;
-            }
-            else {
+            } else {
                 e = mid - 1;
             }
         }
         return ans;
     }
+
+    public static double precision(int temp, int num, int precision) {
+        double factor = 1;
+        double ans = temp;
+
+        for (int i = 0; i < precision; i++) {
+            factor = factor / 10;
+
+            for (double j = ans; j * j < num; j = j + factor) {
+                ans = j;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int n = 139;
-        int root = binarySearch(0,n,n);
-        System.out.println(root);
+        int n = 37;
+        int root = binarySearch(0, n, n);
+        double ans = precision(root, n, 2);
+        System.out.println(ans);
     }
 }
