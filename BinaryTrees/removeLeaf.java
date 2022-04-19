05601b9f3570701d5080bf376baf88861928a3b9
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2022-04-19 20:55:22 +0530

    Remove all leaves from tree.

---
+++ BinaryTrees/removeLeaf.java
@@ -0,0 +1,36 @@
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+ */
+
+import java.util.ArrayList;
+
+public class removeLeaf {
+
+    public static BinaryTreeNode<Integer> removeAllLeaves(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null && root.right == null) {
+            return null;
+        }
+        root.left = removeAllLeaves(root.left);
+        root.right = removeAllLeaves(root.right);
+
+        return root;
+    }
+
+}
