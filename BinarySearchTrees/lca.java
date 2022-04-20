14e870502f83da07b4e71b57def5b941dd3491b6
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2022-04-20 20:03:28 +0530

    LCA of bst.

---
+++ BinarySearchTrees/lca.java
@@ -0,0 +1,46 @@
+/*
+
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+ */
+
+public class Solution {
+
+    static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
+
+        /* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+	 	 * Taking input and printing output is handled automatically.
+        */
+        if(root==null)
+            return -1;
+        if(root.data==a || root.data==b)
+            return root.data;
+        int c=getLCA(root.left,a,b);
+        int d=getLCA(root.right,a,b);
+        if(c == -1 && d == -1)
+            return -1;
+        else if(c == -1 && d != -1)
+            return  d;
+        else if(c != -1 && d == -1)
+            return c;
+        else
+            return root.data;
+
+    }
+
+}
